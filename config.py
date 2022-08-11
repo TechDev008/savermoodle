74cbc2011e13cde5248a11809986fe48f832a848
TechDev008 <97574437+TechDev008@users.noreply.github.com>
2022-08-10 23:09:23 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 10610856
-api_hash = '8a43d39c3eafe0fd50185a1c19017756'
-bot_token = '5464672226:AAGlrDgS9MQvHYryXnogL6o7SRN9W8vfA4Q'
+api_id = 9024532
+api_hash = '131b576240be107210aace99a5f5c5b0'
+bot_token = '5532968445:AAEXpaRpHlXDqGhey4YFrLYD4qRCRFVJZqY'
 useradm = 'studio_apps_dev'
 useradm2 = 'alejandr0123'
 useradm3 = 'Leroy1712'
